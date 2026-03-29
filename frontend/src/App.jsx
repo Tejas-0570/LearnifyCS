820379002078c1fd8a2567d97591fcd58697c9ab
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-30 00:24:56 +0530

    Protected Route Error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './pages/context/Authcontext';
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from './components/Protectedroute';
 
 // Public pages
 import LandingPage from './pages/LandingPage';
