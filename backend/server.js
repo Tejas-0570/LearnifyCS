ea5e58fb74448333e3d6b65da571f56fa61eb4fc
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-30 01:36:26 +0530

    cors error

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 // ── Middleware ────────────────────────────────────────────────────────────────
 app.use(cors({
-  origin: true, //process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://learnifycs-webapp.vercel.app"
+  ],
+  credentials: true
 }));
 app.use(express.json());
 
