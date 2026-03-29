16765c8792a12b3758164fb97f346786b00ce1e0
Tejas-0570 <waydandetejas111@gmail.com>
2026-03-30 01:57:08 +0530

    fix: path-to-regexp error

--- backend/server.js
+++ backend/server.js
@@ -32,14 +32,7 @@ app.use(cors({
   origin: "https://learnifycs-webapp.vercel.app",
   credentials: true
 }));
-app.options("*", cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://learnifycs-webapp.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+app.options("/*", cors());
 
 app.use(express.json());
 
